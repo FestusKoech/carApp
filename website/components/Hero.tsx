2d022c6b0741da9db1279fa34122bb928e136331
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2024-05-15 12:02:35 +0300

    added custom button

---
+++ website/components/Hero.tsx
@@ -0,0 +1,36 @@
+'use client';
+import React from 'react'
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+const Hero = () => {
+
+  const handleClick=() =>{
+
+  }
+  return (
+    <div className='hero'>
+       <div className='flex-1 pt-36 padding-x'>
+
+        <h1 className="hero__title">
+            This is a customizable website using Next-JS, Typescript and React.
+
+        </h1>
+        <p className="hero__subtitle">
+            Change the subtitle to suit your app. This is a reusable app, with 
+            reusable components
+        </p>
+        <CustomButton 
+        title=  'Explore Cars'
+        containerStyles='bg-primary-blue text-white rounded-full mt-10'
+        handleClick={handleClick}
+
+        />
+
+       </div>
+       
+       </div>
+  )
+}
+
+export default Hero
