87b4f03cbbf55fbe7f5b9918565e8050fe7b9f24
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2024-05-19 11:23:32 +0300

    created CarDetails Interface

--- website/components/CarCard.tsx
+++ website/components/CarCard.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
 import { calculateCarRent } from '@/utils';
+import CarDetails from './CarDetails';
 
 
 interface CarCardProps {
@@ -73,6 +74,10 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails isOpen={isOpen} closeModal = {()=> 
+        setIsOpen(false)} car ={car}
+         />
 
 
     </div>
